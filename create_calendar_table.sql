5e5599f98c3247b7901fdd42cc4143a6daa7d3aa
Rodrigo Cardoso <digo.bigdata@outlook.com>
2022-10-06 00:33:34 -0300

    first commits

---
+++ create_calendar_table.sql
@@ -0,0 +1,40 @@
+DECLARE @StartDate  date = '20220101';
+
+DECLARE @CutoffDate date = DATEADD(DAY, -1, DATEADD(YEAR, 30, @StartDate));
+
+;WITH seq(n) AS 
+(
+  SELECT 0 UNION ALL SELECT n + 1 FROM seq
+  WHERE n < DATEDIFF(DAY, @StartDate, @CutoffDate)
+),
+d(d) AS 
+(
+  SELECT DATEADD(DAY, n, @StartDate) FROM seq
+),
+src AS
+(
+  SELECT
+    TheDate         = CONVERT(date, d),
+    TheDay          = DATEPART(DAY,       d),
+    TheDayName      = DATENAME(WEEKDAY,   d),
+    TheWeek         = DATEPART(WEEK,      d),
+    TheISOWeek      = DATEPART(ISO_WEEK,  d),
+    TheDayOfWeek    = DATEPART(WEEKDAY,   d),
+    TheMonth        = DATEPART(MONTH,     d),
+    TheMonthName    = DATENAME(MONTH,     d),
+    TheQuarter      = DATEPART(Quarter,   d),
+    TheYear         = DATEPART(YEAR,      d),
+    TheFirstOfMonth = DATEFROMPARTS(YEAR(d), MONTH(d), 1),
+    TheLastOfYear   = DATEFROMPARTS(YEAR(d), 12, 31),
+    TheDayOfYear    = DATEPART(DAYOFYEAR, d)
+  FROM d
+)
+SELECT *, TheDate AS last_10u_day 
+INTO CALENDAR
+FROM src
+ORDER BY TheDate
+OPTION (MAXRECURSION 0)
+;
+
+SELECT MIN(TheDate), MAX(TheDate), count(1) FROM CALENDAR;
+-- 11 rows
